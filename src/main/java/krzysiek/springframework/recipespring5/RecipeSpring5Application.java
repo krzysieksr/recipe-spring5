a7854373b23414480acadffd6d44f556ac3af092
Krzysztof Sroczyk <krzysztof.sroczyk@ericsson.com>
2017-12-15 14:37:59 +0100

    init message

---
+++ src/main/java/krzysiek/springframework/recipespring5/RecipeSpring5Application.java
@@ -0,0 +1,12 @@
+package krzysiek.springframework.recipespring5;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RecipeSpring5Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RecipeSpring5Application.class, args);
+	}
+}
---
+++ src/test/java/krzysiek/springframework/recipespring5/RecipeSpring5ApplicationTests.java
@@ -0,0 +1,16 @@
+package krzysiek.springframework.recipespring5;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class RecipeSpring5ApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
