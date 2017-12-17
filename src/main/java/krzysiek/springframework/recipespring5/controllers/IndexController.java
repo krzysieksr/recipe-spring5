b5f1abb95dae5fb9cf25ac704e067ab3794c9cfd
Krzysztof Sroczyk <krzysztof.sroczyk@ericsson.com>
2017-12-17 19:53:40 +0100

    7. 91. IntelliJ Compiler Configuration

--- src/main/java/krzysiek/springframework/recipespring5/controllers/IndexController.java
+++ src/main/java/krzysiek/springframework/recipespring5/controllers/IndexController.java
@@ -8,6 +8,7 @@ public class IndexController {
 
     @RequestMapping({"", "/", "index"})
     public String getIndexPage() {
+        System.out.println("Say something...1234");
         return "index";
     }
 }
