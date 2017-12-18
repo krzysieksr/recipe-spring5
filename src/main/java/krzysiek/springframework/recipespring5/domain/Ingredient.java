48a4032d8b4f9d7f5ea2c9fc149a7aa3a0858097
Krzysztof Sroczyk <krzysztof.sroczyk@ericsson.com>
2017-12-18 12:26:01 +0100

    8. 99. Assignment - Create One to One Relationship

--- src/main/java/krzysiek/springframework/recipespring5/domain/Ingredient.java
+++ src/main/java/krzysiek/springframework/recipespring5/domain/Ingredient.java
@@ -17,6 +17,9 @@ public class Ingredient {
     @ManyToOne
     private Recipe recipe;
 
+    @OneToOne(fetch = FetchType.EAGER)
+    private UnitOfMeasure uom;
+
     public Long getId() {
         return id;
     }
@@ -47,5 +50,13 @@ public class Ingredient {
 
     public void setRecipe(Recipe recipe) {
         this.recipe = recipe;
+    }
+
+    public UnitOfMeasure getUom() {
+        return uom;
+    }
+
+    public void setUom(UnitOfMeasure uom) {
+        this.uom = uom;
     }
 }
