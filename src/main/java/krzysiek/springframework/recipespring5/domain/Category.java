6450d2055e64183f76b3bb227241b5a6788e6389
Krzysztof Sroczyk <krzysztof.sroczyk@ericsson.com>
2017-12-18 13:09:35 +0100

    8. 102. Many To Many JPA Relationships

---
+++ src/main/java/krzysiek/springframework/recipespring5/domain/Category.java
@@ -0,0 +1,50 @@
+package krzysiek.springframework.recipespring5.domain;
+
+import javax.persistence.*;
+import java.util.Set;
+
+@Entity
+public class Category {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String description;
+
+    private String CategoryName;
+
+    @ManyToMany(mappedBy = "categories")
+    private Set<Recipe> recipes;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getCategoryName() {
+        return CategoryName;
+    }
+
+    public void setCategoryName(String categoryName) {
+        CategoryName = categoryName;
+    }
+
+    public Set<Recipe> getRecipes() {
+        return recipes;
+    }
+
+    public void setRecipes(Set<Recipe> recipes) {
+        this.recipes = recipes;
+    }
+}
