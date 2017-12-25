bd7dbb60b319709b7aa0ace8b1872a9d9bb2a6d5
Krzysztof Sroczyk <krzysztof.sroczyk@ericsson.com>
2017-12-25 15:06:04 +0100

    11.   138. Create a Recipe

--- src/main/java/krzysiek/springframework/recipespring5/controllers/RecipeController.java
+++ src/main/java/krzysiek/springframework/recipespring5/controllers/RecipeController.java
@@ -1,11 +1,20 @@
 package krzysiek.springframework.recipespring5.controllers;
 
+import krzysiek.springframework.recipespring5.commands.RecipeCommand;
+import krzysiek.springframework.recipespring5.domain.Difficulty;
+import krzysiek.springframework.recipespring5.domain.Recipe;
 import krzysiek.springframework.recipespring5.services.RecipeService;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+
+@RequestMapping("/recipe")
 @Controller
 public class RecipeController {
 
@@ -15,9 +24,27 @@ public class RecipeController {
         this.recipeService = recipeService;
     }
 
-    @RequestMapping("/recipe/show/{id}")
+    @RequestMapping("/show/{id}")
     public String showById(@PathVariable Long id, Model model) {
         model.addAttribute("recipe", recipeService.findById(id));
         return "recipe/show";
     }
+
+    @RequestMapping("/new")
+    public String newRecipe(Model model) {
+        model.addAttribute("recipe", new Recipe());
+        model.addAttribute("difficult", Difficulty.values());
+
+        return "recipe/recipeform";
+    }
+
+    @PostMapping
+    @RequestMapping("/")
+    public String saveOrUpdate(@ModelAttribute RecipeCommand recipeCommand) {
+        RecipeCommand savedCommand = recipeService.saveRecipeCommand(recipeCommand);
+
+        return "redirect:/recipe/show/" + savedCommand.getId();
+    }
+
+
 }
