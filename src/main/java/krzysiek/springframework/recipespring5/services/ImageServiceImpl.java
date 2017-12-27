5d4809becdb782428548f4d4576b265630e959c5
Krzysztof Sroczyk <krzysztof.sroczyk@ericsson.com>
2017-12-27 23:05:16 +0100

    11. 149. Uploading Images with Spring MVC

--- src/main/java/krzysiek/springframework/recipespring5/services/ImageServiceImpl.java
+++ src/main/java/krzysiek/springframework/recipespring5/services/ImageServiceImpl.java
@@ -1,14 +1,46 @@
 package krzysiek.springframework.recipespring5.services;
 
+import krzysiek.springframework.recipespring5.domain.Recipe;
+import krzysiek.springframework.recipespring5.repositories.RecipeRepository;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
 
+import java.io.IOException;
+
 @Slf4j
 @Service
 public class ImageServiceImpl implements ImageService {
+
+    private final RecipeRepository recipeRepository;
+
+    public ImageServiceImpl(RecipeRepository recipeRepository) {
+        this.recipeRepository = recipeRepository;
+    }
+
     @Override
     public void saveImageFile(Long recipeId, MultipartFile file) {
         log.debug("Receive a file");
+
+        try {
+            Recipe recipe = recipeRepository.findById(recipeId).get();
+
+            Byte[] byteObjects = new Byte[file.getBytes().length];
+
+            int i = 0;
+            for (byte b : file.getBytes()) {
+                byteObjects[i++] = b;
+            }
+
+            recipe.setImage(byteObjects);
+
+            recipeRepository.save(recipe);
+
+        } catch (IOException e) {
+            //TODO handle better
+            log.error("Error occurred", e);
+
+            e.printStackTrace();
+        }
     }
 }
---
+++ src/test/java/krzysiek/springframework/recipespring5/services/ImageServiceImplTest.java
@@ -0,0 +1,59 @@
+package krzysiek.springframework.recipespring5.services;
+
+import krzysiek.springframework.recipespring5.domain.Recipe;
+import krzysiek.springframework.recipespring5.repositories.RecipeRepository;
+import org.junit.Before;
+import org.junit.Test;
+import org.mockito.ArgumentCaptor;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+import org.springframework.mock.web.MockMultipartFile;
+
+import java.io.IOException;
+import java.util.Optional;
+
+import static org.junit.Assert.*;
+import static org.mockito.ArgumentMatchers.anyLong;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+
+public class ImageServiceImplTest {
+
+    ImageService imageService;
+
+    @Mock
+    RecipeRepository recipeRepository;
+
+    @Before
+    public void setUp() throws Exception {
+        MockitoAnnotations.initMocks(this);
+        imageService = new ImageServiceImpl(recipeRepository);
+    }
+
+    @Test
+    public void saveImageFile() throws IOException {
+        //given
+        MockMultipartFile multipartFile = new MockMultipartFile("imagefile", "testing.txt",
+                "text/plain", "Test motherfucker".getBytes());
+
+        Long id = 1L;
+        Recipe recipe = new Recipe();
+        recipe.setId(id);
+        Optional<Recipe> recipeOptional = Optional.of(recipe);
+
+        when(recipeRepository.findById(anyLong())).thenReturn(recipeOptional);
+
+        ArgumentCaptor<Recipe> argumentCaptor = ArgumentCaptor.forClass(Recipe.class);
+
+        //when
+        imageService.saveImageFile(id, multipartFile);
+
+        //then
+        verify(recipeRepository, times(1)).save(argumentCaptor.capture());
+        Recipe savedRecipe = argumentCaptor.getValue();
+        assertEquals(multipartFile.getBytes().length, savedRecipe.getImage().length);
+
+
+    }
+}
